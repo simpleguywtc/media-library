e4136e44963919736ae397e7839bacc6d39c2e25
Le Pham Minh Thong <lpmthong@gmail.com>
2017-01-13 22:18:21 +0700

    Adds HomePage components to render home page

---
+++ src/components/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+export default const HomePage = () => {
+  <div className="jumbotron center">
+    <h1 className="lead">Welcome to Media Library built with React, Redux, and Redux-saga </h1>
+    <div>
+      <Link to="library">
+        <button className="btn btn-lg btn-primary"> Visit Library</button>
+      </Link>
+    </div>
+  </div>
+};
